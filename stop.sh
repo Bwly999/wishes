3dee51d8495c446c7ba127cd4c3816dccf063d38
Bwly999 <438225686@qq.com>
2022-06-26 22:02:31 +0800

    add stop.sh

---
+++ stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+function killPro() {
+  name=${1}
+  pid=$(ps ax | grep -i ${name} | grep java | grep -v grep | awk '{print $1}')
+  if [ -n "$pid" ] ; then
+          kill "${pid}"
+          echo "Send shutdown request to ${name}(${pid}) OK"
+  fi
+}
+killPro "gateway"
+killPro "task"
